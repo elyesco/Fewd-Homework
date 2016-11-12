35fe8dbb1da2d53a6abfe15d82eb9922e5fdd0e0
elyesco <elizabethescovar@gmail.com>
2016-11-11 20:13:04 -0500

    Add files via upload
    i really tried to get the state drop down to work but I could not figure it out :( so close!

---
+++ Website/main.js
@@ -0,0 +1,290 @@
+$(document).ready(function() {
+
+var states= [ "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", 
+"Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
+"Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", 
+"Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", 
+"Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", 
+ "Wyoming" ];
+
+var sel= document.getElementById("state-type");
+
+for(var i=0; i <= 50; i++) {
+$("#state-type").append($("<option>" + states[i] + "</option>"))
+}
+
+$("#state-type").change(function () {
+var states =$("#state-type").val()
+
+if (states==="Alabama") {
+	$("url").link("http://www.coveredca.com/");
+}
+
+else if (states==="Alaska") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Arizona") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Arkansa") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="California") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Colorado") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Connecticut") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Delaware") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="District of Columbia") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Florida") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Georgia") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Hawaii") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Idaho") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Illinois") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Indiana") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Iowa") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Kansas") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Kentucky") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Louisiana") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Maine") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Maryland") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Massachusetts") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Michigan") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Massachusetts") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Michigan") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Minnesota") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Mississippi") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Missouri") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Montana") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Nebraska") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Nevada") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="New Hampshire") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="New Jersey") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="New Mexico") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="New York") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="North Carolina") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="North Dakota") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Ohio") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Oklahoma") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Oregon") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Pennsylvania") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Rhode Island") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="South Carolina") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="South Dakota") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Tennessee") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Texas") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Utah") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Vermont") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Virginia") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Washington") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="West Virginia") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Wisconsin") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+else if (states==="Wyoming") {
+	$("url").link("http://www.coveredca.com/");
+
+}
+
+
+});
+
+});
+
+
+
+
